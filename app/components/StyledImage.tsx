555b30b704b6f0c4cda9131408df28cd0970d036
Sebastián Capelli <scapelli@fi.uba.ar>
2025-02-09 18:56:22 -0300

    linter

--- app/components/StyledImage.tsx
+++ app/components/StyledImage.tsx
@@ -4,12 +4,11 @@ import { Box, Typography } from "@mui/material";
 import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/autoplay";
 
 interface StyledImageProps {
   sources: string[];
--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,10 +1,9 @@
 "use client";
 import StyledBox from "./StyledBox";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
 
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import { Sections } from "../Constants/Sections";
