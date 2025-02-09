1e2bae53a90679838475b2de6a76975bebfd83b3
Sebastián Capelli <scapelli@fi.uba.ar>
2025-02-09 14:21:55 -0300

    bottom padding

--- app/sections/Services.tsx
+++ app/sections/Services.tsx
@@ -26,7 +26,7 @@ export function Services() {
         textAlign: "center",
         alignItems: "center",
         justifyContent: "center",
-
+        paddingBottom: "160px",
         alignSelf: "center",
         justifySelf: "center",
       }}
